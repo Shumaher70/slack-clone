a3155381ea4504ba33adbaa53dea1d34ce67b003
Oleg Vrabiy <olegvraby@gmail.com>
2024-09-17 17:31:56 +0200

    update:app/page.tsx: UserButton instead of Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
-
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
+import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
   return (
     <div>
-      Logged in
-      <Button onClick={() => signOut()}>Sign out</Button>
+      <UserButton />
     </div>
   );
 }
