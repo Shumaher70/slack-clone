4b485d6fb41eead0ce3007d07797b971cb7f72ea
Oleg Vrabiy <olegvraby@gmail.com>
2024-09-20 18:16:04 +0200

    update:user-button.tsx: change path to useCurrentUser component

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuContent,
 } from "@/components/ui/dropdown-menu";
 
-import { useCurrentUser } from "../hooks/use-current-user";
+import { useCurrentUser } from "../api/use-current-user";
 import { useAuthActions } from "@convex-dev/auth/react";
 
 export const UserButton = () => {
