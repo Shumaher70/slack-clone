dc7a6ed29d987075bccbba3b608ff6f3e8b1d7b3
Oleg Vrabiy <olegvraby@gmail.com>
2024-09-20 18:13:47 +0200

    create:workspaceIdPage: initial page has been created

---
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -0,0 +1,10 @@
+interface WorkspaceIdPageProps {
+  params: {
+    workspaceId: string;
+  };
+}
+
+const WorkspaceIdPage = ({ params }: WorkspaceIdPageProps) => {
+  return <div>ID: {params.workspaceId}</div>;
+};
+export default WorkspaceIdPage;
