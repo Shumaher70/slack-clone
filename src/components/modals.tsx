a9815f677932bf1e3bd48cda7c00d4d71a90a41c
Oleg Vrabiy <olegvraby@gmail.com>
2024-09-20 18:06:28 +0200

    create:modals.tsx: Wrapper for all modals

---
+++ src/components/modals.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { CreateWorkspaceModal } from "@/features/auth/workspaces/components/create-workspace-modal";
+
+export const Modal = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <>
+      <CreateWorkspaceModal />
+    </>
+  );
+};
