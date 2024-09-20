a7f4bf15f3d7642b1308d366357ea54ad998c1f4
Oleg Vrabiy <olegvraby@gmail.com>
2024-09-20 18:09:15 +0200

    create:use-create-workspaces.ts: Logic for creating workspaces and logic for handle all status

---
+++ src/features/auth/workspaces/api/use-create-workspaces.ts
@@ -0,0 +1,65 @@
+import { useCallback, useMemo, useState } from "react";
+import { useMutation } from "convex/react";
+
+import { api } from "../../../../../convex/_generated/api";
+import { Id } from "../../../../../convex/_generated/dataModel";
+
+type RequestType = { name: string };
+type ResponseType = Id<"workspaces"> | null;
+
+type Options = {
+  onSuccess?: (data: ResponseType) => void;
+  onError?: (error: Error) => void;
+  onSettled?: () => void;
+  throwError?: boolean;
+};
+
+const useCreateWorkspaces = () => {
+  const [data, setData] = useState<ResponseType>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [status, setStatus] = useState<
+    "success" | "error" | "settled" | "pending" | null
+  >(null);
+
+  const isError = useMemo(() => status === "error", [status]);
+  const isPending = useMemo(() => status === "pending", [status]);
+  const isSuccess = useMemo(() => status === "success", [status]);
+  const isSettled = useMemo(() => status === "settled", [status]);
+
+  const mutation = useMutation(api.workspaces.create);
+
+  const mutate = useCallback(
+    async (values: RequestType, options?: Options) => {
+      try {
+        setData(null);
+        setError(null);
+        setStatus("pending");
+
+        const response = await mutation(values);
+        options?.onSuccess?.(response);
+        return response;
+      } catch (error) {
+        options?.onError?.(error as Error);
+        if (options?.throwError) {
+          throw error;
+        }
+      } finally {
+        setStatus("settled");
+        options?.onSettled?.();
+      }
+    },
+    [mutation],
+  );
+
+  return {
+    data,
+    error,
+    mutate,
+    isError,
+    isPending,
+    isSuccess,
+    isSettled,
+  };
+};
+
+export default useCreateWorkspaces;
