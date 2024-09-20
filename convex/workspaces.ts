dbe3e0d87328e05f56fd9d0b6c34f9c4fcf04e71
Oleg Vrabiy <olegvraby@gmail.com>
2024-09-20 17:53:51 +0200

    create: workspaces api such as create and get have been added

---
+++ convex/workspaces.ts
@@ -0,0 +1,35 @@
+import { v } from "convex/values";
+
+import { auth } from "./auth";
+import { mutation, query } from "./_generated/server";
+
+export const create = mutation({
+  args: {
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    // TODO: Create a proper method later
+    const joinCode = "123456";
+
+    const workspaceId = await ctx.db.insert("workspaces", {
+      name: args.name,
+      userId,
+      joinCode,
+    });
+
+    return workspaceId;
+  },
+});
+
+export const get = query({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.query("workspaces").collect();
+  },
+});
