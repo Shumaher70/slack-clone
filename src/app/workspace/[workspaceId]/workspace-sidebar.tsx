e9fc0751e50c647d892214935624043d8009830d
Oleg Vrabiy <olegvraby@gmail.com>
2024-09-27 19:57:30 +0200

    create:workspace-sidebar:

---
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -0,0 +1,45 @@
+import { AlertTriangle, Loader } from "lucide-react";
+
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useCurrentMember } from "@/features/members/api/use-current-member";
+
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+import { WorkspaceHeader } from "./workspace-header";
+
+export const WorkspaceSidebar = () => {
+  const workspaceId = useWorkspaceId();
+
+  const { data: member, isLoading: memberLoading } = useCurrentMember({
+    workspaceId,
+  });
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+
+  if (workspaceLoading || memberLoading) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center bg-[#5E2C5F]">
+        <Loader className="size-5 animate-spin text-white" />
+      </div>
+    );
+  }
+
+  if (!workspace || !member) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center gap-y-2 bg-[#5E2C5F]">
+        <AlertTriangle className="size-5 text-white" />
+        <p className="text-sm text-white">Workspace not found</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex h-full flex-col bg-[#5E2C5F]">
+      <WorkspaceHeader
+        workspace={workspace}
+        isAdmin={member.role === "admin"}
+      />
+    </div>
+  );
+};
