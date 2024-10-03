396b08304ec56a3f949f4f4c60b53b7a82915a1f
Oleg Vrabiy <olegvraby@gmail.com>
2024-10-03 17:31:59 +0200

    update:use-create-workspaces.ts: SetStatus("error")

--- src/features/workspaces/api/use-create-workspaces.ts
+++ src/features/workspaces/api/use-create-workspaces.ts
@@ -39,6 +39,7 @@ const useCreateWorkspaces = () => {
         options?.onSuccess?.(response);
         return response;
       } catch (error) {
+        setStatus("error");
         options?.onError?.(error as Error);
         if (options?.throwError) {
           throw error;
