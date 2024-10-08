32d60cf505fd6a636c95b0df26e0d97b5ff6d117
Oleg Vrabiy <olegvraby@gmail.com>
2024-10-08 19:49:54 +0200

    create:create-channel-modal.tsx:

---
+++ src/features/channels/components/create-channel-modal.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogHeader,
+  DialogContent,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+import { useCreateChannel } from "../api/use-create-channel";
+import { useCreateChannelModal } from "../store/use-create-channel-modal";
+
+export const CreateChannelModal = () => {
+  const workspaceId = useWorkspaceId();
+  const { mutate, isPending } = useCreateChannel();
+  const [open, setOpen] = useCreateChannelModal();
+
+  const [name, setName] = useState("");
+
+  const handleClose = () => {
+    setName("");
+    setOpen(false);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\s+/g, "-").toLocaleLowerCase();
+    setName(value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate(
+      {
+        name,
+        workspaceId,
+      },
+      {
+        onSuccess: (id) => {
+          //TODO: Redirect to new channel
+          handleClose();
+        },
+      },
+    );
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a channel</DialogTitle>
+        </DialogHeader>
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <Input
+            required
+            autoFocus
+            value={name}
+            minLength={3}
+            maxLength={80}
+            disabled={isPending}
+            onChange={handleChange}
+            placeholder="e.g. plan-budget"
+          />
+          <div className="flex justify-end">
+            <Button disabled={isPending}>Create</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
