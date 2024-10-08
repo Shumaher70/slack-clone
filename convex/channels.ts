3ac76c3c77eb202d9b1b1d78023ac153bc687d55
Oleg Vrabiy <olegvraby@gmail.com>
2024-10-08 19:48:03 +0200

    update:convex/channels.ts: CREATE method

--- convex/channels.ts
+++ convex/channels.ts
@@ -1,7 +1,41 @@
 import { v } from "convex/values";
 
 import { auth } from "./auth";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
+
+export const create = mutation({
+  args: {
+    name: v.string(),
+    workspaceId: v.id("workspaces"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId),
+      )
+      .unique();
+
+    if (!member || member.role !== "admin") {
+      throw new Error("Unauthorized");
+    }
+
+    const parsedName = args.name.replace(/\s+/g, "-").toLocaleLowerCase();
+
+    const channelId = await ctx.db.insert("channels", {
+      name: parsedName,
+      workspaceId: args.workspaceId,
+    });
+
+    return channelId;
+  },
+});
 
 export const get = query({
   args: {
