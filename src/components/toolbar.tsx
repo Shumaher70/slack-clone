fd051c831a7ae48f4ea760aaf2d0519365683442
Oleg Vrabiy <olegvraby@gmail.com>
2024-10-29 18:22:16 +0200

    create:toolbar.tsx

---
+++ src/components/toolbar.tsx
@@ -0,0 +1,61 @@
+import { MessageSquareTextIcon, Pencil, Smile, Trash } from "lucide-react";
+
+import { Hint } from "./hint";
+import { Button } from "./ui/button";
+import { EmojiPopover } from "./emoji-popover";
+
+interface ToolbarProps {
+  isAuthor: boolean;
+  isPending: boolean;
+  handleEdit: () => void;
+  handleThread: () => void;
+  handleDelete: () => void;
+  handleReaction: (value: string) => void;
+  hideThreadButton?: boolean;
+}
+
+export const Toolbar = ({
+  isAuthor,
+  isPending,
+  handleEdit,
+  handleThread,
+  handleDelete,
+  handleReaction,
+  hideThreadButton,
+}: ToolbarProps) => {
+  return (
+    <div className="absolute right-5 top-0">
+      <div className="rounded-md border bg-white opacity-0 shadow-sm transition-opacity group-hover:opacity-100">
+        <EmojiPopover
+          hint="Add reaction"
+          onEmojiSelect={(emoji) => handleReaction(emoji.native)}
+        >
+          <Button variant="ghost" size="iconSm" disabled={isPending}>
+            <Smile className="size-4" />
+          </Button>
+        </EmojiPopover>
+        {!hideThreadButton && (
+          <Hint label="Replay in thread">
+            <Button variant="ghost" size="iconSm" disabled={isPending}>
+              <MessageSquareTextIcon className="size-4" />
+            </Button>
+          </Hint>
+        )}
+        {isAuthor && (
+          <Hint label="Edit">
+            <Button variant="ghost" size="iconSm" disabled={isPending}>
+              <Pencil className="size-4" />
+            </Button>
+          </Hint>
+        )}
+        {isAuthor && (
+          <Hint label="Delete message">
+            <Button variant="ghost" size="iconSm" disabled={isPending}>
+              <Trash className="size-4" />
+            </Button>
+          </Hint>
+        )}
+      </div>
+    </div>
+  );
+};
