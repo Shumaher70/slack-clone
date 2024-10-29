1cbb1fa2167502f8da68e95a7be90917061ba5fe
Oleg Vrabiy <olegvraby@gmail.com>
2024-10-29 17:20:25 +0200

    create:channel-hero.tsx

---
+++ src/components/channel-hero.tsx
@@ -0,0 +1,19 @@
+import { format } from "date-fns";
+
+interface ChannelHeroProps {
+  name: string;
+  creationTime: number;
+}
+
+export const ChannelHero = ({ name, creationTime }: ChannelHeroProps) => {
+  return (
+    <div className="mx-5 mb-4 mt-[88px]">
+      <p className="mb-2 flex items-center text-2xl font-bold"># {name}</p>
+      <p className="mb-4 font-normal text-slate-800">
+        This channel was created on{" "}
+        {format(new Date(creationTime), "MMMM do, yyyy")}. This is the very
+        beginning of the <strong>{name}</strong> channel.
+      </p>
+    </div>
+  );
+};
