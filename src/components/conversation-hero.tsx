991a5682d6e928e699f66e1bf736825823082356
Oleg Vrabiy <olegvraby@gmail.com>
2024-11-07 17:57:26 +0200

    create:conversation-hero.tsx

---
+++ src/components/conversation-hero.tsx
@@ -0,0 +1,28 @@
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+interface ConversationHeroProps {
+  name?: string;
+  image?: string;
+}
+
+export const ConversationHero = ({
+  name = "Member",
+  image,
+}: ConversationHeroProps) => {
+  const avatarFallback = name.charAt(0).toUpperCase();
+
+  return (
+    <div className="mx-5 mb-4 mt-[88px]">
+      <div className="mb-2 flex items-center gap-x-1">
+        <Avatar className="mr-2 size-14">
+          <AvatarImage src={image} />
+          <AvatarFallback>{avatarFallback}</AvatarFallback>
+        </Avatar>
+        <p className="text-2xl font-bold">{name}</p>
+      </div>
+      <p className="mb-4 font-normal text-slate-800">
+        This conversation is just between you and <strong>{name}</strong>
+      </p>
+    </div>
+  );
+};
