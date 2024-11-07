fa4676f131873d7ff77a52da6f0443e47aa0abfa
Oleg Vrabiy <olegvraby@gmail.com>
2024-11-07 19:46:37 +0200

    update:thread.tsx:Additional props threadName for a message component has been added

--- src/features/messages/components/thread.tsx
+++ src/features/messages/components/thread.tsx
@@ -211,6 +211,7 @@ export const Thread = ({ messageId, onClose }: ThreadProps) => {
                   hideThreadButton
                   threadCount={message.threadCount}
                   threadImage={message.threadImage}
+                  threadName={message.threadName}
                   threadTimestamp={message.threadTimestamp}
                 />
               );
