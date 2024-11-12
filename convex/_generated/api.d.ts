4ff808cf0c9bfd5688edacd39bf33127280487e7
Oleg Vrabiy <olegvraby@gmail.com>
2024-11-12 18:13:23 +0200

    deploy

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,7 +13,16 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as tasks from "../tasks.js";
+import type * as auth from "../auth.js";
+import type * as channels from "../channels.js";
+import type * as conversations from "../conversations.js";
+import type * as http from "../http.js";
+import type * as members from "../members.js";
+import type * as messages from "../messages.js";
+import type * as reactions from "../reactions.js";
+import type * as upload from "../upload.js";
+import type * as users from "../users.js";
+import type * as workspaces from "../workspaces.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -24,7 +33,16 @@ import type * as tasks from "../tasks.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  tasks: typeof tasks;
+  auth: typeof auth;
+  channels: typeof channels;
+  conversations: typeof conversations;
+  http: typeof http;
+  members: typeof members;
+  messages: typeof messages;
+  reactions: typeof reactions;
+  upload: typeof upload;
+  users: typeof users;
+  workspaces: typeof workspaces;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
