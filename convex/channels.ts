b593485bdda93d3e584608437ba5c4a522d3a57c
Oleg Vrabiy <olegvraby@gmail.com>
2024-11-12 17:05:18 +0200

    update:channels.ts:  Clean up messages for Remove function

--- convex/channels.ts
+++ convex/channels.ts
@@ -31,7 +31,16 @@ export const remove = mutation({
       throw new Error("Unauthorized");
     }
 
-    // TODO: Remove associated messages
+    const [messages] = await Promise.all([
+      ctx.db
+        .query("messages")
+        .withIndex("by_channel_id", (q) => q.eq("channelId", args.id))
+        .collect(),
+    ]);
+
+    for (const message of messages) {
+      await ctx.db.delete(message._id);
+    }
 
     await ctx.db.delete(args.id);
 
